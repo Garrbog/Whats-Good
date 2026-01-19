59e6c70f9329e617ff4991dad81a3d3c40b91d4c
William Ford <garrettford02@gmail.com>
2026-01-19 13:40:29 -0700

    Initialize SwiftUI Xcode project

---
+++ WhatsGood/WhatsGood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsGood
+//
+//  Created by William Garrett Ford on 1/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsGood/WhatsGood/WhatsGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsGoodApp.swift
+//  WhatsGood
+//
+//  Created by William Garrett Ford on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WhatsGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
